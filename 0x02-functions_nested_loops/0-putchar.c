418c34d9601c7f8dadebf77576927857941da26b
Kwame Acheampong-Cobbiah <acobby99@gmail.com>
2023-06-20 10:04:45 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - check the code 
+ *
+ * Return: Always 0
+ *
+ */
+int main(void)
+{
+	int i = 0;
+	char p[] = "_putchar";
+
+	for (i < 8)
+		{
+		_putchar(p[i]);
+		i++;
+		}
+		_putchar('\n');
+	return (0);
+}
