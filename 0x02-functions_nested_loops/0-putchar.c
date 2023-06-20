3598bd9bdb7730db7da5bdf38108f7db554ad122
Kwame Acheampong-Cobbiah <acobby99@gmail.com>
2023-06-20 12:25:13 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,8 @@
 #include "main.h"
 /**
 * main - print _putchar
+* @ i: first int
+* @p: char
 *
 * Return: 0
 */
@@ -9,7 +11,7 @@ int main(void)
 int i;
 	char p[] = "_putchar";
 
-	for (i = 0; i < 9; i++)
+	for (i = 0; i < 8; i++)
 		{
 		_putchar(p[i]);
 		}
