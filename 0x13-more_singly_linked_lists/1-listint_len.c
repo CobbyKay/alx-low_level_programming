47bc79a12f483a98a18d973f870a7bdb66f0f41e
Kwame Acheampong-Cobbiah <acobby99@gmail.com>
2023-08-01 12:26:33 +0000

    1-listint_len.c

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+* listint_len - returns the number of elements in a linked listint_t list
+* @h: singly linked list
+* Return: number of nodes
+*/
+size_t listint_len(const listint_t *h)
+{
+size_t node_count;
+
+while (h != NULL)
+{
+node_count++;
+h = h->next;
+}
+return (node_count);
+}
+
