6dfb9c1cc52625914e003bd5fec9027d64a96297
Kwame Acheampong-Cobbiah <acobby99@gmail.com>
2023-08-01 14:13:08 +0000

    101-print_listint_safe.c new

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -8,25 +8,6 @@
 */
 size_t print_listint_safe(const listint_t *head)
 {
-const listint_t *slow = head;
-const listint_t *fast = head;
-size_t count = 0;
-
-while (slow && fast && fast->next)
-{
-printf("%d\n", slow->n);
-count++;
-
-slow = slow->next;
-fast = fast->next->next;
-
-if (slow == fast)
-{
-printf("-> [%p] %d\n", (void *)slow, slow->n);
-break;
+head++;
+return (0);
 }
-}
-
-return (count);
-}
-
