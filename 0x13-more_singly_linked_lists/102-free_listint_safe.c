80c776e90b0eb7222c5b7dc17851aeccc4d065ae
Kwame Acheampong-Cobbiah <acobby99@gmail.com>
2023-08-01 14:27:48 +0000

    103-find_loop.c

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,12 +1,30 @@
 #include "lists.h"
+
 /**
-* free_listint_safe - function that frees a listint_t list
-* @h: free
-* Return: return listint
+* free_listint_safe - Frees a listint_t linked list safely.
+* @h: Double pointer to the head of the list.
+* Return: The total number of nodes freed.
 */
 size_t free_listint_safe(listint_t **h)
 {
-h++;
-return (0);
+listint_t *current = *h;
+listint_t *temp;
+size_t count = 0;
+
+while (current != NULL)
+{
+temp = current;
+current = current->next;
+free(temp);
+count++;
+
+if (current == *h)
+{
+*h = NULL;
+break;
+}
+}
+
+return (count);
 }
 
