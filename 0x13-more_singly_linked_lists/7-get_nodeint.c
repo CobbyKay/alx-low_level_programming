c008a2bad14081961785245d66a06dcde9a46acd
Kwame Acheampong-Cobbiah <acobby99@gmail.com>
2023-08-01 13:18:41 +0000

    7-get_nodeint.c new

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -9,11 +9,14 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 unsigned int i;
 
-for (i = 0; (i < index; i++)
+i = 0;
+
+while (i < index)
 {
-if (head == NULL)
+if (head->next == NULL)
 return (NULL);
 head = head->next;
+i++;
 }
 return (head);
 }
