a4671bdee84166828808d24b5061d66fc3092565
Kwame Acheampong-Cobbiah <acobby99@gmail.com>
2023-08-03 15:24:54 +0000

    flip bits fixed

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,18 +1,24 @@
 #include "main.h"
+
 /**
-* flip_bits - returns the number of bits you would need to flip
+* flip_bits - counts the number of bits to change
 * to get from one number to another
-* @n: base number
-* @m: number to transform to
-* Return: the number of bit transformations needed
+* @n: first number
+* @m: second number
+* Return: returns the number of bits to change
 */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-int i, res;
+int i, c = 0;
+unsigned long int current;
+unsigned long int xor = n ^ m;
 
-result = 0;
-for (i = 8 * sizeof(n) - 1; i >= 0; i--)
-if (((n ^ m) >> i) & 1)
-result++;
-return (result);
+for (i = 63; i >= 0; i--)
+{
+current = xor >> i;
+if (current & 1)
+c++;
+}
+
+return (c);
 }
